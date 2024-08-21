8df0edb22eafd79bfbf081ec20c00f6e62a301b4
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-21 14:45:22 -0500

    Done

--- W1-E3/Program.cs
+++ W1-E3/Program.cs
@@ -4,28 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace W1-E3
+namespace Excercise3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int totalInterest;
+            int PA = 0;
+            double rate = 0.0;
+            int time = 0;
+            int interest = 0;
 
+            //Title of program
             Console.WriteLine("Interest Calculator Program");
 
+            //Prompts user to input PA of loan
             Console.WriteLine("What is the principal amount of the loan in dollars?");
-            string PA = Console.ReadLine();
+            //Converts user input to int
+            PA = Convert.ToInt32(Console.ReadLine());
 
+            //Prompts user to input interest
             Console.WriteLine("What is interest rate (input 0.05 for 5%)?");
-            string interest = Console.ReadLine();
+            //Converts user input to double
+            rate = Convert.ToDouble(Console.ReadLine());
 
+            //Prompts user to input time of loan
             Console.WriteLine("What is the period of loan in years?");
-            string time = Console.ReadLine();
+            //Converts user input to int
+            time = Convert.ToInt32(Console.ReadLine());
 
-            PA* interest *time = totalInterest
+            //Calculation of interest overall
+            interest = Convert.ToInt32(PA * rate * time);
 
-            Console.WriteLine("Total interest of Loan: " + totalInterest + "dollars.")
+            //Displays to user total interest 
+            Console.WriteLine($"Total interest of Loan: {interest} dollars.");
+            Console.ReadKey();
         }
     }
 }
