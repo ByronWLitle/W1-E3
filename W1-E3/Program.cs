24322330889c8ee40d54d20a306725863e7e5f79
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-21 13:25:57 -0500

    si

---
+++ W1-E3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1-E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalInterest;
+
+            Console.WriteLine("Interest Calculator Program");
+
+            Console.WriteLine("What is the principal amount of the loan in dollars?");
+            string PA = Console.ReadLine();
+
+            Console.WriteLine("What is interest rate (input 0.05 for 5%)?");
+            string interest = Console.ReadLine();
+
+            Console.WriteLine("What is the period of loan in years?");
+            string time = Console.ReadLine();
+
+            PA* interest *time = totalInterest
+
+            Console.WriteLine("Total interest of Loan: " + totalInterest + "dollars.")
+        }
+    }
+}
